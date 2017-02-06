a799652f895e318768855385892875b9ddbfb0ee
shinabarger <davidshinabarger@gmail.com>
2017-02-06 09:19:42 -0500

    added verification & comments

--- src/com/jetbrains/Main.java
+++ src/com/jetbrains/Main.java
@@ -7,12 +7,22 @@ public class Main {
     public static void main(String[] args) {
         String answer;
         Scanner scan = new Scanner(System.in);
-        long userNumber = scan.nextLong();
-        long initialNumber = userNumber;
+
         long factorialNumber = 0;
 
         do {
             System.out.println("Please enter any number between 1 & 25 you'd like a factorial of. ");
+            long userNumber = scan.nextLong();
+            long initialNumber = userNumber;
+
+            //IF number > 25 OR less than 1
+            if (userNumber > 25 || userNumber < 1) {
+                System.out.println("Oops! That wasn't between 1 & 25.");
+                System.out.println("Let's try again. Please enter any number between 1 & 25 you'd like a factorial of. ");
+                userNumber = scan.nextLong();
+                initialNumber = userNumber;
+
+            }
 
             //get the usernumber and get the factorial of it with this method
             factorialNumber = getFactorialNumber(userNumber, factorialNumber);
@@ -22,7 +32,8 @@ public class Main {
 
             //asks user if they want to continue
             answer = wantsToContinue(scan);
-            if (answer == null) break;
+            if (answer == null)
+                break;
         }
         while (answer.equalsIgnoreCase("Y"));
 
